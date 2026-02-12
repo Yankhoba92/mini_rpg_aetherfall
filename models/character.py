23000e5acae7cdd64791bde398c6cf764754a797
Bradley Mutima <98081808+poky789456@users.noreply.github.com>
2026-02-12 22:01:15 +0100

    takeItem et useItem

--- models/character.py
+++ models/character.py
@@ -1,15 +1,46 @@
 
+from typing import List
+
+from items import Item
+
+
 class Character:
     list_statut = []
 
 
 class Player(Character):
+
     def __init__(self, name, x=0, y=0):
         self.name = name
         self.x = x
         self.y = y
+        self.inventory: List[Item] = []
 
     def move(self, dx, dy):
         self.x += dx
         self.y += dy
         print(f"[{self.name}] Position actuelle : (x={self.x}, y={self.y})")
+    
+    def useItem(self, item: Item):
+        if item in self.inventory:
+            if isinstance(item, Item):
+                if item.consommable:
+                    self.inventory.remove(item)
+                print(f"{self.name} a utilisé {item.name}!")
+            else:
+                print(f"{item.name} n'est pas un objet utilisable.")
+        else:
+            print(f"{item.name} n'est pas dans l'inventaire de {self.name}.")
+
+    def takeItem(self, item: Item):
+        self.inventory.append(item)
+        print(f"{self.name} a pris {item.name}!")
+
+if __name__ == "__main__":
+    player = Player("Hero")
+    potion = Item("potion", True)
+    player.takeItem(potion)
+    print(f"Inventaire de {player.name} : {[item.name for item in player.inventory]}")
+    player.useItem(potion)
+
+    print(f"Inventaire de {player.name} : {[item.name for item in player.inventory]}")
--- models/items.py
+++ models/items.py
@@ -21,11 +21,18 @@ class Antidote(Item):
         print(f"Hero a utilisé une {self.name} et a été guéri de l'empoisonnement!")
 
 class Arme(Item):
-    def __init__(self, name: str = "arme", consommable: bool = False):
+    def __init__(self, name: str, consommable: bool = False, atk: int = 0, magic: int = 0):
         super().__init__(name, consommable)
+        self.attack = atk
+        self.magic_attack = magic
     
-    def attack(self, player, target):
-        print(f"Hero a utilisé une {self.name} pour attaquer {target.name}!")
+class Armure(Item):
+    def __init__(self, name: str, consommable: bool = False, defense: int = 0, magic_defense: int = 0):
+        super().__init__(name, consommable)
+        self.defense = defense
+        self.magic_defense = magic_defense
+
+
 
 class Bombe(Item):
     def __init__(self, name: str = "bombe", consommable: bool = True):
@@ -38,21 +45,27 @@ class Bombe(Item):
 class ItemFactory(Item):
     
     @staticmethod
-    def createItem(name: str, consommable: bool):
+    def createItem(name: str, consommable: bool, atk: int = 0, magic: int = 0, defense: int = 0, magic_defense: int = 0):
         if name == "potion":
             return Potion(name, consommable)
         elif name == "antidote":
             return Antidote(name, consommable)
         elif name == "bombe":
             return Bombe(name, consommable)
+        elif name == "arme":
+            return Arme(name, consommable, atk, magic)
+        elif name == "armure":
+            return Armure(name, consommable, defense, magic_defense)
+        elif name == "item":
+            return Item(name, consommable)
         else: 
             raise ValueError("Invalid item name")
     
 
 
 if __name__ == "__main__":
-    potion = ItemFactory.createItem("potion", True)
-    potion.heal("Hero")
+    potion = ItemFactory.createItem("arme", False, atk=10, magic=5)
+    print(f"Crée l'item: {potion.name}, Attack: {potion.attack}, Magic: {potion.magic_attack}")
 
 
 
