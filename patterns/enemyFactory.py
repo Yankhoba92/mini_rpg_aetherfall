673c41964e41126e6d242c89782e7841956ede48
Yankhoba92 <y.bagayoko@h3hitema.fr>
2026-02-12 19:46:58 +0100

    feat(enemy): ajout d'énumeration pour enemy types

---
+++ patterns/enemyFactory.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class EnemyType(Enum):
+    Loup_sauvage = "loup_sauvage"
+    Bandit = "bandit"
+    Squelette = "squelette"
+    Champion_corrompu = "champion_corrompu"
+    Gardien_donjon = "gardien_donjon"
